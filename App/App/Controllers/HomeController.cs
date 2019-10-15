35311a1b70853eebe135159de66167be0c99bc02
Trung <Trungghost.win@gmail.com>
2019-10-15 23:26:42 +0700

    # 3 update export excel

--- App/App/Controllers/HomeController.cs
+++ App/App/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using App.Models;
+using OfficeOpenXml;
+using System.Globalization;
+
 
 namespace App.Controllers
 {
@@ -11,7 +14,7 @@ namespace App.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             List<Employee> lstEmployee = new List<Employee>();
 
@@ -21,16 +24,42 @@ namespace App.Controllers
             lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
             lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
 
-            ViewData["employee"] = lstEmployee;
 
-            return View();
+            return View(lstEmployee);
         }
 
-        public ActionResult About()
+        public void DownloadExcel()
         {
-            ViewBag.Message = "Your app description page.";
 
-            return View();
+            List<Employee> lstEmployee = new List<Employee>();
+
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+
+            ExcelPackage Ep = new ExcelPackage();
+            ExcelWorksheet Sheet = Ep.Workbook.Worksheets.Add("Report");
+            Sheet.Cells["A1"].Value = "Name";
+            Sheet.Cells["B1"].Value = "Address";
+            Sheet.Cells["C1"].Value = "Birthday";
+            int row = 2;
+
+            foreach (var item in lstEmployee)
+            {
+
+                Sheet.Cells[string.Format("A{0}", row)].Value = item.Name;
+                Sheet.Cells[string.Format("B{0}", row)].Value = item.Address;
+                Sheet.Cells[string.Format("C{0}", row)].Value = item.Birthday.ToString();
+                row++;
+            }
+            Sheet.Cells["A:AZ"].AutoFitColumns();
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment: filename=" + "Report.xlsx");
+            Response.BinaryWrite(Ep.GetAsByteArray());
+            Response.End();
         }
 
         public ActionResult Contact()
