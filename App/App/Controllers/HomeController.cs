ce300416602f910cbd010684e1ab66905276f476
Minh Tien <huynhminhtien0104@gmail.com>
2019-10-15 21:05:41 +0700

    #2 Add new demo project

---
+++ App/App/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App.Models;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            List<Employee> lstEmployee = new List<Employee>();
+
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+            lstEmployee.Add(new Employee() { Name = "admin1", Address = "Bản đồ Tp HCM, bản đồ các quận, map tphcm mới nhất", Birthday = DateTime.Now });
+
+            ViewData["employee"] = lstEmployee;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ App/App/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Models
+{
+    public class Employee
+       
+    {
+        public String Name { get; set; }
+        public String Address { get; set; }  
+        public DateTime Birthday { get; set; }
+
+    }
+}
